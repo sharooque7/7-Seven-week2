0aed7bdd6bc6753c1b036740eee8d722b2f1960d
sharooque7 <52906045+sharooque7@users.noreply.github.com>
2021-02-24 13:40:55 +0530

    Add files via upload

---
+++ Script_Total_Population.js
@@ -0,0 +1,17 @@
+var request = new XMLHttpRequest();
+
+request.open('GET',"https://restcountries.eu/rest/v2/all",true);
+
+request.send();
+
+request.onload= function()
+{
+    var countryData = JSON.parse(this.response);
+    console.log(countryData);
+    var total_population = 0;
+    for(var i in countryData)
+    {
+        total_population += +countryData[i].population;
+    }
+    console.log("ToTal_Poplation"+'  '+total_population);
+};
